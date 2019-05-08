629f3a448e2cc1fa36926dba1488371b8c5d35bf
Sunny Dhama <31814079+blackwolf08@users.noreply.github.com>
2019-05-08 17:04:17 +0530

    css

--- src/components/homeComponents/ImageAvatars.js
+++ src/components/homeComponents/ImageAvatars.js
@@ -30,22 +30,13 @@ function ImageAvatars(props) {
   return (
     <div>
       <div className="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div className="modal-dialog" role="document">
+        <div className="mydialog modal-dialog" role="document">
           <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+            <div className="mymodal"></div>
               <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
               </button>
             </div>
-            <div className="modal-body">
-              ...
-            </div>
-            <div className="modal-footer">
-              <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-              <button type="button" className="btn btn-primary">Save changes</button>
-            </div>
-          </div>
         </div>
       </div>
       <div style={styles.root}>
