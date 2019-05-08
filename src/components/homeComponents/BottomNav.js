ee78ac37a9cac1eeaa5b6098852419dcf8da257d
Sunny Dhama <31814079+blackwolf08@users.noreply.github.com>
2019-05-08 14:47:30 +0530

    production ready

--- src/components/homeComponents/BottomNav.js
+++ src/components/homeComponents/BottomNav.js
@@ -8,11 +8,10 @@ const styles = {
     display: 'flex',
     justifyContent: 'space-around',
     alignItems: 'center',
-    position: 'sticky',
+    position: 'fixed',
     bottom: 0,
     backgroundColor: '#fff',
     height: '50px',
-    position:"-webkit-sticky"
   },
   button: {
     backgroundColor: 'transparent',
@@ -41,10 +40,10 @@ class BottomNav extends React.Component {
 
     return (
       <div value={value} onChange={this.handleChange} style={styles.root}>
-        <i style={{color: 'black'}} className="fas fa-home"></i>
-        <i className="fas fa-search"></i>
-        <i className="far fa-plus-square"></i>
-        <i className="far fa-heart"></i>
+        <button style={styles.button} onClick={this.logout}><i style={{color: 'black'}} className="fas fa-home"></i></button>
+        <button style={styles.button} onClick={this.logout}><i className="fas fa-search"></i></button>
+        <button style={styles.button} onClick={this.logout}><i className="far fa-plus-square"></i></button>
+        <button style={styles.button} onClick={this.logout}><i className="far fa-heart"></i></button>
         <button style={styles.button} onClick={this.logout}><i className="far fa-user"></i></button>
       </div>
     );
